40fa14e5842b51ae5ea781ec4ecc98f50855e6d7
Leocadio <guilhermeleocadio1301@gmail.com>
2025-02-02 19:12:56 -0300

    exercises 1001 and 1003

---
+++ src/main/kotlin/1001.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+
+    val A: String = sc.next()
+    val B: String = sc.next()
+
+    println("X = " + (A.toInt() + B.toInt()))
+
+
+}
