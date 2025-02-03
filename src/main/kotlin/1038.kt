63c39f4861167b0ee92c05e9dc91f3bb01819170
Leocadio <guilhermeleocadio1301@gmail.com>
2025-02-02 21:13:59 -0300

    exercise 1038 and 1059

---
+++ src/main/kotlin/1038.kt
@@ -0,0 +1,21 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val codigo = scanner.nextInt()
+    val quantidade = scanner.nextInt()
+
+    val preco = when(codigo) {
+        1 -> 4.00
+        2 -> 4.50
+        3 -> 5.00
+        4 -> 2.00
+        5 -> 1.50
+        else -> 0.00
+    }
+
+    val total = preco * quantidade
+
+    println("%.2f".format(total))
+
+}
---
+++ src/main/kotlin/1059.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    for (i in 1..100) {
+        if ( i % 2 == 0) {
+            println(i)
+        }
+    }
+}
